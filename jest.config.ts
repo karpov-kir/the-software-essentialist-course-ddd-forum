389e18b8700a7d05c0a0f2dc6eda2571f7f054bb
Kirill Karpov <karpov.kir.v@gmail.com>
2023-08-07 14:35:14 +0400

    Add support of imports from ESM files to Jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,17 @@
-import { Config } from 'jest';
+import type { JestConfigWithTsJest } from 'ts-jest';
 
-export default async (): Promise<Config> => ({
+// https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+export default async (): Promise<JestConfigWithTsJest> => ({
   verbose: true,
+  preset: 'ts-jest/presets/default-esm',
   testMatch: ['**/*.@(spec|ispec|steps|e2e).*'],
+  resolver: '<rootDir>/jest.mjsResolver.cjs',
   transform: {
-    '^.+\\.m?(t|j)s$': ['ts-jest', {}],
+    '^.+\\.m?[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
 });
